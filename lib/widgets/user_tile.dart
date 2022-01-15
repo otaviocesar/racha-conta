c2540daf635cbbd5ce2c52b3a070323079e16909
Otavio Cesar <o.caesar@hotmail.com>
2022-01-15 20:45:43 -0300

    Implementando botão de exclusão de usuário

--- lib/widgets/user_tile.dart
+++ lib/widgets/user_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rachaconta/models/user.dart';
+import 'package:provider/provider.dart';
+import 'package:rachaconta/providers/users.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -21,7 +23,30 @@ class UserTile extends StatelessWidget {
               IconButton(
                   icon: const Icon(Icons.delete),
                   color: Colors.red,
-                  onPressed: () {}),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                              title: const Text('Excluir Usuário'),
+                              content: const Text('Tem certeza???'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: const Text('Não'),
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(false),
+                                ),
+                                TextButton(
+                                  child: const Text('Sim'),
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(true),
+                                ),
+                              ],
+                            )).then((confirmed) {
+                      if (confirmed) {
+                        Provider.of<Users>(context, listen: false).remove(user);
+                      } //if
+                    });
+                  }),
               IconButton(
                   icon: const Icon(Icons.quiz),
                   color: Colors.greenAccent,
